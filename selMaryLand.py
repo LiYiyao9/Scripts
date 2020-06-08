bde9588b2e1f334b0e1af0498099c7831602f9dc
LiYiyao9 <40173003+LiYiyao9@users.noreply.github.com>
2020-06-08 16:05:54 +0800

    Update selMaryLand.py

--- selMaryLand.py
+++ selMaryLand.py
@@ -2,13 +2,8 @@ import time
 from time import sleep
 import os.path
 from random import randint
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait  # available since 2.4.0
-# available since 2.26.0
-from selenium.webdriver.support import expected_conditions as EC
+
 
 url = "http://www.city-data.com/so/Maryland.html"
 links = []
